07b0d1019457bcd8455e0178cdc5ed14a8d441c4
Stefan_Bodoarca <bodoarcastefan@yahoo.ro>
2023-04-04 20:05:10 +0300

    test's examples from chapter 6

---
+++ Java/class-design/src/main/java/com/ro/inheritance/test/Owl.java
@@ -0,0 +1,17 @@
+package com.ro.inheritance.test;
+
+abstract class Nocturnal {
+    abstract boolean isBlind();
+}
+
+public class Owl extends Nocturnal {
+    @Override
+    public boolean isBlind() {
+        return false;
+    }
+
+    public static void main(String[] args) {
+        var nocturnal = new Owl();
+        System.out.println(nocturnal.isBlind());
+    }
+}
